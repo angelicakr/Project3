0945a15d8efe015da614d939f1b7ee564f23456d
Kayla Jordan <kaygrajor@gmail.com>
2018-12-13 21:48:02 -0700

    basic compoent structure started to work on

---
+++ create-react-express/client/src/Components/stickies/index.js
@@ -0,0 +1,9 @@
+import React, {
+  Component
+} from 'react';
+import Container from "../Container";
+import Photos from "../Photos";
+import Header from "../Header";
+import data from "../../data";
+
+
