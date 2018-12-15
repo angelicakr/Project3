1c658a2b344644606d45d15aaa582a0cf8967303
unknown <angelicakarina.rivas@gmail.com>
2018-12-15 16:05:48 -0700

    second

--- create-react-express/client/src/App.js
+++ create-react-express/client/src/App.js
@@ -1,21 +1,7 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component} from 'react';
+import Stickies2 from "./Components/Stickies2/Stickies2.js" 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() { return (<Stickies2/>)}
 }
-
 export default App;
